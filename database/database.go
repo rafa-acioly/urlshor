d10678c80f4071043fd25356ad7411679e7a9d72
Rafael Acioly <aciolyr@gmail.com>
2018-06-05 16:23:23 -0300

    add documentation on database and redis packages

--- database/database.go
+++ database/database.go
@@ -1,13 +1,17 @@
+// Package database is a encapsulation for postgres driver
 package database
 
+// GetLastInsertedID return the next value for column "id" using "next_val"
 func GetLastInsertedID() (uint64, error) {
-	return "", nil
+	return 123, nil
 }
 
+// Insert makes a insert query on database
 func Insert(encode, url string) error {
-	return true, nil
+	return nil
 }
 
+// Query makes a select statement on database
 func Query(encode string) {
 
 }
--- redis/redis.go
+++ redis/redis.go
@@ -1,9 +1,15 @@
+// Package redis is a encapsulation for go-redis package
+//
+// The key is represented by a encoded ID used on database
+// it means that decoding the key you will retrieve the ID for the register on database
 package redis
 
-func get(encode string) {
+// Get retrieve the URL for given encoded ID
+func Get(encode string) {
 
 }
 
-func set(encode, url string) {
+// Set saves the given URL for a encoded ID
+func Set(encode, url string) {
 
 }
