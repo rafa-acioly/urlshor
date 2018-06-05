2183dc8601962c3e1c4b2a1bedb2c3a6a249af0b
Rafael Acioly <aciolyr@gmail.com>
2018-06-05 11:03:20 -0300

    base structure initialized :rocket:

---
+++ base36.go
@@ -0,0 +1,53 @@
+package magaursho
+
+import "math"
+
+var (
+	base36 = []byte{
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+		'U', 'V', 'W', 'X', 'Y', 'Z'}
+
+	index = map[byte]int{
+		'0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
+		'5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+		'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14,
+		'F': 15, 'G': 16, 'H': 17, 'I': 18, 'J': 19,
+		'K': 20, 'L': 21, 'M': 22, 'N': 23, 'O': 24,
+		'P': 25, 'Q': 26, 'R': 27, 'S': 28, 'T': 29,
+		'U': 30, 'V': 31, 'W': 32, 'X': 33, 'Y': 34,
+		'Z': 35,
+		'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14,
+		'f': 15, 'g': 16, 'h': 17, 'i': 18, 'j': 19,
+		'k': 20, 'l': 21, 'm': 22, 'n': 23, 'o': 24,
+		'p': 25, 'q': 26, 'r': 27, 's': 28, 't': 29,
+		'u': 30, 'v': 31, 'w': 32, 'x': 33, 'y': 34,
+		'z': 35,
+	}
+)
+
+// Encode encodes a number to base36
+func encode36(value uint64) string {
+
+	var res [16]byte
+	var i int
+	for i = len(res) - 1; value != 0; i-- {
+		res[i] = base36[value%36]
+		value /= 36
+	}
+	return string(res[i+1:])
+}
+
+// Decode decodes a base36-encoded string
+func decode36(s string) uint64 {
+
+	res := uint64(0)
+	l := len(s) - 1
+	for idx := range s {
+		c := s[l-idx]
+		byteOffset := index[c]
+		res += uint64(byteOffset) * uint64(math.Pow(36, float64(idx)))
+	}
+	return res
+}
---
+++ magaursho.go
@@ -0,0 +1,48 @@
+package magaursho
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/short", shortURL)
+	router.HandleFunc("/get/{id}", getURL)
+
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "5000"
+	}
+
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
+
+func shortURL(w http.ResponseWriter, r *http.Request) {
+	// Check if the request have a valid URL
+	// if not valid return a http.StatusNotAllowed
+
+	// Get the last inserted ID and sum +1 to find out which is the next ID to be inserted on database
+
+	// Generate a encode with base36 on the (last inserted ID + 1)
+
+	// Save the URL and the encode on database
+
+	// Save the URL and the encoded on redis
+
+	// return the new encoded URL
+}
+
+func getURL(w http.ResponseWriter, r *http.Request) {
+	// Check if the id is on redis and return the url if found
+
+	// Check if the id is on database and return the url if found
+
+	// If we do not find the ID, show a 404 page
+
+	// redirect to the URL
+}
