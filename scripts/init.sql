9e9cf0e71b6d03edb459a0cdea79b7ebf5e61967
Rafael Acioly <aciolyr@gmail.com>
2018-06-11 14:35:12 -0300

    fix postgres init sql

--- scripts/init.sql
+++ scripts/init.sql
@@ -2,8 +2,9 @@ create database urlshor;
 \c urlshor
 
 create table urls(
-	`id` serial not null,
-	`url` varchar(255) not null,
-	`clicks` integer not null,
-	`created_at` timestamp not null default current_timestamp;
+	id integer not null,
+	url varchar(255) not null,
+	clicks integer not null,
+	created_at timestamp not null default current_timestamp,
+	constraint pk_urls_id primary key (id)
 );
