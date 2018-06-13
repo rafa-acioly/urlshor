21cb733c4ea9fc3e7ee5b40592786f813a306b43
Rafael Acioly <aciolyr@gmail.com>
2018-06-13 10:29:49 -0300

    :wrench: fix sql file

--- scripts/init.sql
+++ scripts/init.sql
@@ -7,7 +7,7 @@ create table urls(
 	encoded varchar(255) not null UNIQUE,
 	clicks integer DEFAULT 0,
 	created_at timestamp not null DEFAULT current_timestamp,
-	constraint pk_urls_id primary key (id),
+	constraint pk_urls_id primary key (id)
 );
 
 create sequence seq_urls_id;
